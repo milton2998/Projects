40106115ce860c5b07e1216ec25727ec0602fb4f
khandakoustav <khandakoustav@gmail.com>
2020-06-16 19:12:29 +0530

    My first commit on Projects  folder

---
+++ Text to Speech/code.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+import os
+
+# add text in the file if needed
+# f = open('text_for_speech.txt')
+print("Write the text here : ")
+x = input()
+language = 'en'
+
+audio = gTTS(text=x,lang=language)
+audio.save('generated_audio.wav')
+
+os.system('vlc --play-and-exit generated_audio.wav')
